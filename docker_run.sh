7c75f56fd51f1301d464f3da022d35ff8d3e401b
Anton Schegg <anton@schegg.de>
2024-01-06 11:59:42 +0100

    Muiltiplatform docker image build

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,4 @@
 docker run --env=PORT_HTTP=8000 --env=PORT_HTTPS=8008 \
  --env="SSL_CERT_SUBJ=/C=DE/ST=Bavaria/O=Anton Schegg/CN=finanzkraft.schegg.net"\
  -v /Users/anton/finanzkraft_data:/data -p 8008:8008 -p 8000:8000 \
- -d ghcr.io/as19git67/finanzkraft:main
+ -d ghcr.io/as19git67/finanzkraft:latest
