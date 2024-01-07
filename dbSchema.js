1aad326866a3867bc0b284c86a443a656fd6a5c2
Anton Schegg <anton@schegg.de>
2024-01-07 16:50:02 +0100

    DB Schema für Währung, Konto und Transaktionen

--- dbSchema.js
+++ dbSchema.js
@@ -1,6 +1,17 @@
 const schema = [
   {
-    "tableName": "Gattungen",
+    "tableName": "Fk_Currency",
+    "columns": [
+      {
+        "name": "name",
+        "type": "string",
+        length: 3,
+        "primary_key": true,
+      },
+    ],
+  },
+  {
+    "tableName": "Fk_Account",
     "columns": [
       {
         "name": "id",
@@ -8,7 +19,13 @@ const schema = [
         "primary_key": true,
       },
       {
-        "name": "Name",
+        "name": "name",
+        "type": "string",
+        "unique": true,
+        "nullable": false,
+      },
+      {
+        "name": "iban",
         "type": "string",
         "unique": true,
         "nullable": false,
@@ -16,7 +33,7 @@ const schema = [
     ],
   },
   {
-    "tableName": "Tiere",
+    "tableName": "Fk_Category",
     "columns": [
       {
         "name": "id",
@@ -24,22 +41,102 @@ const schema = [
         "primary_key": true,
       },
       {
-        "name": "Name",
+        "name": "name",
         "type": "string",
-        "unique": true,
         "nullable": false,
       },
       {
-        "name": "idGattung",
+        "name": "idCategoryParent",
         "type": "integer",
-      }
+        "nullable": true,
+      },
+    ],
+    indexes: [
+      {
+        name: 'IDX_name_idCategoryParent',
+        columns: ['name', 'idCategoryParent'],
+        unique: true,
+      },
     ],
     "foreign_keys": [
       {
-        "name": "FK_idGattung__Gattungen_id",
-        "columns": ["idGattung"],
-        "foreign_table": "Gattungen",
+        "name": "FK_idCategoryParent__Fk_Category_id",
+        "columns": ["idCategoryParent"],
+        "foreign_table": "Fk_Category",
         "foreign_columns": ["id"],
+      }
+    ],
+  },
+  {
+    "tableName": "Fk_Transaction",
+    "columns": [
+      {
+        "name": "id",
+        "type": "autoincrement",
+        "primary_key": true,
+      },
+      {
+        "name": "idAccount",
+        "type": "integer",
+        "nullable": false,
+      },
+      {
+        "name": "bookingDate",
+        "type": "dateTime",
+        "nullable": false,
+      },
+      {
+        "name": "valueDate",
+        "type": "dateTime",
+        "nullable": false,
+      },
+      {
+        "name": "amount",
+        "type": "decimal",
+        precision: 12,
+        scale: 2,
+        "nullable": false,
+      },
+      {
+        "name": "amountCurrency",
+        "type": "string",
+        "nullable": false,
+        length: 3,
+      },
+      {
+        "name": "text",
+        "type": "string",
+        "nullable": true,
+      },
+      {
+        "name": "notes",
+        "type": "string",
+        "nullable": true,
+      },
+      {
+        "name": "idCategory",
+        "type": "integer",
+        "nullable": true,
+      },
+    ],
+    "foreign_keys": [
+      {
+        "name": "FK_idAccount__Fk_Account_id",
+        "columns": ["idAccount"],
+        "foreign_table": "Fk_Account",
+        "foreign_columns": ["id"],
+      },
+      {
+        "name": "FK_idCategory__Fk_Category_id",
+        "columns": ["idCategory"],
+        "foreign_table": "Fk_Category",
+        "foreign_columns": ["id"],
+      },
+      {
+        "name": "FK_amountCurrency__Fk_Currency_name",
+        "columns": ["amountCurrency"],
+        "foreign_table": "Fk_Currency",
+        "foreign_columns": ["name"],
       }
     ],
   },
