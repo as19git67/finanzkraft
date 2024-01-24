5df77bae78a5016338bf2b18ae74a828f2b01190
Anton Schegg <aschegg@opentext.com>
2024-01-24 20:56:49 +0100

    fix packages and comment

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,6 +1,5 @@
 import { AsRouteConfig } from 'as-express';
 
-//export default new AsRouteConfig().get('/gattung/', function (req, res, next) {
 export default new AsRouteConfig('/').get(function (req, res, next) {
   const db = req.app.get('database');
   db.getAccounts().then((accounts) =>{
