5962271381589d6a18f944d019bde3e5c615dc65
Anton Schegg <anton@schegg.de>
2024-03-02 19:02:25 +0100

    update ruleset

--- routes/rule.js
+++ routes/rule.js
@@ -17,21 +17,34 @@ rc.get(function (req, res, next) {
   });
 });
 
-rc.post(function (req, res, next) {
+rc.post(async function (req, res, next) {
   const {idRuleSet} = req.params;
   if (idRuleSet === undefined) {
     res.send(404);
     return;
   }
-  const ruleInfo = req.body;
+  const { ruleInfo, includeProcessed } = req.body;
   ruleInfo.id = idRuleSet;
+  //const includeProcessed = req.body.includeProcessed !== undefined ? req.body.includeProcessed : false;
   const db = req.app.get('database');
-  db.updateRuleSet(ruleInfo).then(() => {
+
+  try {
+    await db.updateRuleSet(ruleInfo);
+    console.log(`RuleSet with idRuleSet ${idRuleSet} updated in DB`);
+    await db.applyRules({idRuleSet: idRuleSet, includeProcessed: includeProcessed, includeTransactionsWithRuleSet: true});
+    console.log(`RuleSet applied to transactions`);
     res.sendStatus(200);
-  }).catch((reason) => {
-    console.log(reason);
-    res.sendStatus(500);
-  });
+  } catch (error) {
+    switch (error.cause) {
+      case 'exists':
+        console.error(error.message);
+        res.sendStatus(422);
+        break;
+      default:
+        console.error(error);
+        res.sendStatus(500);
+    }
+  }
 });
 
 rc.delete(function (req, res, next) {
