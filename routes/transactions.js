252817aed3076af7d1734f30c2829bec605c94f6
Anton Schegg <anton@schegg.de>
2024-03-14 17:42:19 +0100

    package update; status code instead of error page on HtmlError; PUT transaction rest call

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,8 @@
 import {AsRouteConfig} from 'as-express';
 
-const rc = new AsRouteConfig('/').get(function (req, res, next) {
+const rc = new AsRouteConfig('/');
+
+rc.get(function (req, res, next) {
   const db = req.app.get('database');
   const maxItems = req.query.maxItems;
   const searchTerm = req.query.searchTerm;
@@ -17,5 +19,35 @@ const rc = new AsRouteConfig('/').get(function (req, res, next) {
     res.sendStatus(500);
   });
 });
+
+rc.put(async (req, res, next) => {
+  const transactionData = req.body;
+  const db = req.app.get('database');
+  try {
+    const result = await db.addTransaction(transactionData);
+    if (result.length > 0) {
+      const id = result[0];
+      console.log(`transaction created with id ${id}`);
+      res.sendStatus(200, id);
+    } else {
+      throw new Error('Unable to create transaction');
+    }
+  } catch (error) {
+    switch (error.cause) {
+      case 'exists':
+        console.error(error.message);
+        res.sendStatus(422);
+        break;
+      default:
+        console.error(error);
+        if (error.message) {
+          res.send(500, error.message);
+        } else {
+          res.sendStatus(500);
+        }
+    }
+  }
+});
+
 
 export default rc;
