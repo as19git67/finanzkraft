8f6af1f1ef8113b6a7257570e3d6d14f5058b659
anton <anton@schegg.de>
2025-06-14 20:18:30 +0200

    fix db specific raw query

--- dbMixinAccounts.js
+++ dbMixinAccounts.js
@@ -6,38 +6,44 @@ const DbMixinAccounts = {
   },
 
   async getAccounts() {
-    // const latestBalances = await this.knex.table('Fk_AccountBalance')
-    //   .select(['idAccount', 'balance'])
-    //   .max('balanceDate', { as: 'balanceDate' })
-    //   .groupBy('idAccount')
-    //   .as('latestBalances');
-
-    return this.knex.select().table('Fk_Account')
-    .join('Fk_Currency', function () {
+    return this.knex
+    .with('numbered_balances', (qb) => {
+      qb.select('*').rowNumber('row_number', function () {
+        this.orderBy('balanceDate', 'desc').partitionBy('idAccount');
+      }).from('Fk_AccountBalance');
+    })
+    .with('last_balances', (qb) => {
+      qb.select('*')
+      .from('numbered_balances')
+      .where('row_number', 1);
+    })
+    .select( [
+      'Fk_Account.id as id',
+      'Fk_Account.name',
+      'Fk_Account.iban',
+      'Fk_Account.number',
+      'Fk_Account.startBalance',
+      'Fk_Account.idAccountType as account_type_id',
+      'Fk_Account.closedAt',
+      'Fk_Currency.id as currency_id',
+      'Fk_Currency.name as currency_name',
+      'Fk_Currency.short as currency_short',
+      'last_balances.balanceDate',
+      'last_balances.balance',
+      'reader',
+      'writer'
+    ])
+    .from('Fk_Account')
+    .leftJoin('last_balances', function() {
+      this.on('Fk_Account.id', '=', 'last_balances.idAccount');
+    })
+    .leftJoin('Fk_Currency', function () {
       this.on('Fk_Account.idCurrency', '=', 'Fk_Currency.id');
     })
-    .leftJoin(this.knex.table('Fk_AccountBalance')
-      .select(['idAccount', 'balance'])
-      .max('balanceDate', { as: 'balanceDate' })
-      .groupBy('idAccount')
-      .as('latestBalances'), 'latestBalances.idAccount', 'Fk_Account.id')
-    .leftJoin('FK_AccountReader', function () {
-      this.on('FK_AccountReader.idAccount', '=', 'Fk_Account.id');
-    })
-    .leftJoin('FK_AccountWriter', function () {
-      this.on('FK_AccountWriter.idAccount', '=', 'Fk_Account.id');
-    })
+    .leftJoin(this.knex.raw("(select FK_AccountReader.idAccount, STRING_AGG(FK_AccountReader.idUser, ',') as reader from Fk_AccountReader group by idAccount) as Fk_AccountReader_agg on Fk_Account.id = Fk_AccountReader_agg.idAccount"))
+    .leftJoin(this.knex.raw("(select FK_AccountWriter.idAccount, STRING_AGG(FK_AccountWriter.idUser, ',') as writer from FK_AccountWriter group by idAccount) as FK_AccountWriter_agg on Fk_Account.id = FK_AccountWriter_agg.idAccount"))
     .orderBy('closedAt', 'asc')
-    .orderBy('name', 'asc')
-    .groupBy('Fk_Account.id')
-    .select(['Fk_Account.id as id', 'Fk_Account.name as name', 'Fk_Account.iban as iban',
-      'Fk_Account.number as number', 'Fk_Account.startBalance as startBalance', 'Fk_Account.idAccountType as account_type_id',
-      'latestBalances.balanceDate', 'latestBalances.balance',
-      'Fk_Currency.id as currency_id', 'Fk_Currency.name as currency_name',
-      'Fk_Currency.short as currency_short', 'Fk_Account.closedAt as closedAt',
-      this.supportsGroupConcat() ? this.knex.raw('GROUP_CONCAT(distinct FK_AccountReader.idUser) as reader') : this.knex.raw("STRING_AGG(FK_AccountReader.idUser, ',') as reader"),
-      this.supportsGroupConcat() ? this.knex.raw('GROUP_CONCAT(distinct FK_AccountWriter.idUser) as writer') : this.knex.raw("STRING_AGG(FK_AccountWriter.idUser, ',') as writer"),
-    ])
+    .orderBy('Fk_Account.name', 'asc')
   },
 
   async addAccount(accountData) {
